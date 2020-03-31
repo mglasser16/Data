45aa2b20a380b2df925ed542e20df6446ae6125c
Melodie <mglasser@mines.edu>
2020-03-31 14:50:56 -0600

    Added minimum function to make the split for graphs easier

--- data_process_3.py
+++ data_process_3.py
@@ -1,8 +1,11 @@
+#%%
+import cantera as ct
 import pandas as pd
 import numpy as np
 import io
 import matplotlib.pyplot as plt
 import os
+import re
 
 #Reference https://www.fuelcellstore.com/avcarb-mgl190
 
@@ -10,7 +13,7 @@ carb_area=(9**2)*np.pi #in mm^2
 carb_v =  carb_area*0.19 #in mm^3
 carb_den = 0.44*.001 #in g/mm^3
 carb_por = 0.78 #percentage
-carb_m=carb_v*carb_den*carb_por
+carb_m=carb_v*carb_den*(1-carb_por)
 
 def graph(file):
     D = pd.read_table(file, sep='deliminator', engine='python', header=None)
@@ -23,23 +26,18 @@ def graph(file):
     D[1] = D[1].astype(float)
     D[2] = D[2].astype(float)
     D[3] = D[3].astype(float)
-    
-
 
     D['capacity'] = D[1] * abs(D[3])/carb_m*1000/3600 #convert to mAh/g
-    title = file.split("_cycle")
+    title = re.split('_cycle|.DTA',file)
     title2 = title[1]
-    
-    for cell in D[2]:
-        if cell > D.iloc[0,2]:
-            row = list(D[2]).index(cell)
-            newc=D.iloc[row:,2]
-            D['adjust']=newc
-            D['charge']=D['adjust'].shift(-1*row)
-            break
-   
-    D['discharge'] =D[2].where(D[2]<=D.iloc[0,2]) 
-            
+
+    min =D[2].min()
+    row2 = list(D[2]).index(min)
+    newc=D.iloc[row2:,2]
+    D['adjust']=newc
+    D['charge']=D['adjust'].shift(-1*row2)
+    D['discharge'] =D[2].loc[0:row2]
+
 # =============================================================================
 #     if D.iloc[1,2] < 0:
 #         D['absolute'] = D[2]*-1
@@ -56,17 +54,17 @@ def graph(file):
 #         plt.xlabel('time (s)', fontsize=12)
 #         plt.ylabel('voltage (V)', fontsize=12)
 # =============================================================================
-        
+
     plt.figure(0)
     plt.scatter(D['capacity'], D['charge'], marker='o', label = title2 +"charge")
     plt.scatter(D['capacity'], D['discharge'], marker='o', label = title2 +"discharge")
     plt.xlabel('Capacity (mAh/g)', fontsize=12)
     plt.ylabel('voltage (V)', fontsize=12)
-    plt.legend(framealpha=1, frameon=True);
-    
-    
+    plt.legend(framealpha=1, frameon=False);
+
+
     D['Voltgap'] = D['charge'] -D['discharge']
-    
+
     Voltgap = D.Voltgap.mean()
     Voltgap2 = 5
     return [Voltgap, Voltgap2]
@@ -76,26 +74,34 @@ def graph(file):
 #    plt.xlabel('time (s)', fontsize=12)
 #    plt.ylabel('current (A)', fontsize=12)
 
-path = 'C:/Users/Amy LeBar/Documents/Data'
+path = 'C:/Users/pjsch/Documents/Data'
 
 i=0
 iform =[]
 V = []
 V2 = []
 
-for file in os.listdir('C:/Users/Amy LeBar/Documents/Data'):
-    if file.find('08012019') > -1:
-        Voltgap =graph(path + "/"+ file)
-        print(Voltgap)
-        V.append(Voltgap[0])
-        V2.append(Voltgap[1])
-        iform.append(i)
-        i=i+1
-   
-    
-Report = pd.DataFrame()  
-Report['V1'] = V
-Report['V2'] = V2
-plt.figure(1)
-plt.scatter(iform, Report['V1'], marker='o')
 
+for file in os.listdir('C:/Users/pjsch/Documents/Data'):
+    if file.find('20200310') > -1:
+        Voltgap = graph(path + "/"+ file)
+        title = re.split('_cycle|.DTA',file)
+        i= int(title[1])
+#        V[0] =Voltgap[0]
+#        print(V)
+#        print(Voltgap)
+
+#        V.append(Voltgap[0])
+#        V2.append(Voltgap[1])
+#        iform.append(i)
+
+
+
+# Report = pd.DataFrame()
+# Report['V1'] = V
+# Report['V2'] = V2
+# plt.figure(1)
+# plt.scatter(iform, Report['V1'], marker='o')
+
+
+# %%
